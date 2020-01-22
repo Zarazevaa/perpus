e15689812edd58ee82c0672ac23dbf6a1a36fbab
zarazevaa <zara_zeva_27rpl@student.smktelkom-mlg.sch.id>
2020-01-22 20:03:48 +0700

    JWT laravel

--- app/User.php
+++ app/User.php
@@ -1,39 +1,36 @@
 <?php
-
 namespace App;
-
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+use Notifiable;
+/**
+* The attributes that are mass assignable.
+*
+* @var array
+*/
+protected $table = 'petugas';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+protected $fillable = [
+'nama_petugas', 'alamat', 'telp', 'username', 'password', 'level',
+];
+/**
+* The attributes that should be hidden for arrays.
+*
+* @var array
+*/
+protected $hidden = [
+'password', 'remember_token',
+];
+public function getJWTIdentifier()
+{
+return $this->getKey();
+}
+public function getJWTCustomClaims()
+{
+return [];
+}
 }
